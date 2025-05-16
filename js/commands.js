c4e810788f5d706ed42951355f408486b2a20573
Niraj <59787504+nirajpoudel@users.noreply.github.com>
2025-05-16 14:56:38 -0700

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,101 @@
+
+let linkedin = 'https://www.linkedin.com/in/mrniraj';
+let github = 'https://github.com/nirajpoudel';
+let website = 'https://github.com/nirajpoudel/nirajpoudel/blob/master/Niraj%20Chudali_V.7.pdf';
+let instagram = 'https://www.instagram.com/nj.pdl';
+let project_site = ""
+let existCommands = [
+  'about',
+  'banner',
+  'clear',
+  'cls',
+  'date',
+  'github',
+  'resume',
+  'help',
+  'history',
+  'linkedin',
+  'project',
+  'repo',
+  'social',
+  'theme',
+  'theme ls',
+  'theme set',
+  'twitter',
+  'whoami',
+  'cd ..'
+];
+
+about = [
+  "&nbsp;",
+  "&nbsp;",
+  "<span style='color:red;'>01101000 01101001</span> There 👋, I'm <span style='color: yellow;'>Niraj</span> ",
+  "&nbsp;",
+  "Motivated software engineer with strong academic background and professional experience aiming to excel in the field of Software",
+  "engineering and AI. Proven ability to manage cross-functional projects, implement cutting-edge technologies, and drive impactful business",
+  "outcomes. I exhibit creativity, adaptability, and a strong work ethic.",
+  "&nbsp;",
+  "&nbsp;",
+  'Connect With me 💫',
+'&nbsp;',
+// '<p align="center">',
+
+'<a href="https://linkedin.com/in/mrniraj" target="blank"><img align="center" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-1024.png" alt="niraj" height="30" width="30" /></a> <a href="https://www.instagram.com/nj.pdl" target="blank"><img align="center" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png" alt="niraj" height="30" width="30" /></a>&nbsp<a href="https://github.com/nirajpoudel" target="blank"><img align="center" src="https://icones.pro/wp-content/uploads/2021/06/icone-github-orange.png" alt="utkar5hm" height="30" width="30" /></a>',
+
+
+'&nbsp;',
+'&nbsp;',
+];
+
+social = [
+  'TO open the below social profiles Type the desired one!',
+  'Example : github',
+  '----------------',
+  '&nbsp;',
+  '<a href="https://www.linkedin.com/in/mrniraj" style="color: skyblue;">Linkedin</a>',
+  '<a href="https://www.instagram.com/nj.pdl" style="color: skyblue;">Instagram</a>',
+  '<a href="https://github.com/nirajpoudel" style="color: skyblue;">Github</a>',
+];
+
+
+themes = ['dark', 'light'];
+
+theme = [
+  'Usage: theme [arg]',
+  'Args:',
+  ' - ls: list all available themes',
+  ' - set: set a theme',
+  '<br>',
+  'Example:',
+  ' theme ls # to list all available themes',
+  ' <span style="color:skyblue;">theme set light</span> # to set a theme',
+];
+
+help = [
+  '<span class="command">about  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        - </span>     <span class="helpdes">About Me</span>',
+  '<span class="command">social &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - </span>  <span class="helpdes">   Connect with Me</span>',
+  '<span class="command">resume  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  - </span> <span class="helpdes">View Resume</span>',
+  '<span class="command">banner &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - </span> <span class="helpdes">Display the header</span>',
+  '<span class="command">help  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;         - </span>     <span class="helpdes">Check available commands</span>',
+  '<span class="command">history &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      - </span> <span class="helpdes">    View command history</span>',
+];
+
+banner = [
+'<span style="color: yellowgreen;">visitor@terminal.niraj:~$</span> Welcome home!!',
+' &nbsp;',
+' &nbsp;',
+' ',
+
+'Niraj',
+
+'&nbsp;',
+'&nbsp;',
+" <span style='color:red;'>01101000 01101001</span>👋, I'm <span style='color: yellow;'>Niraj</span> ",
+'Software Engineer',
+' - - - -',
+'&nbsp;',
+
+'To Know more about myself, type<span class="command">\'help\'</span> ',
+
+];
+
